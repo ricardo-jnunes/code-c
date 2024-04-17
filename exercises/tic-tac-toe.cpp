5c0ee410a365a0a924f770b324ab3eddeb039e5c
Ricardo J. Nunes <jnunes.ricardo@gmail.com>
2024-04-17 13:57:28 -0300

    fix: correct word in code comment

--- exercises/tic-tac-toe.cpp
+++ exercises/tic-tac-toe.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 #include<string>
 using namespace std;
-// Função para mostrar possições da tabela
+// FunÃ§Ã£o para mostrar posiÃ§Ãµes da tabela
 int mapa(int i){
     cout << endl << endl;
     cout << "Mapa de possicoes" << endl;
@@ -12,7 +12,7 @@ int mapa(int i){
     cout << " 7 | 8 | 9   " << endl;
     cout << endl;
     }
-    // Função para mostrar tabela
+    // FunÃ§Ã£o para mostrar tabela
 void mapa_mod(char matriz[3][3]){
     cout << endl << endl;
     cout << "Mapa do Jogo"<<endl;
@@ -23,7 +23,7 @@ void mapa_mod(char matriz[3][3]){
     cout << " " << matriz[0][0]<< " | "<< matriz[0][1]<<" | " << matriz[0][2] << endl;
     cout << endl;
 }
-    //Função para verificar se o numero digitado é repetido
+    //FunÃ§Ã£o para verificar se o numero digitado Ã© repetido
 int verifica_posicao(char velha[3][3], int posicao){
         int cont = 0;
         if(posicao == '1')
@@ -69,17 +69,17 @@ int main(){
             velha[i][j] = ' ';
         }
     }
-    // Faz o laço enquanto alguem vença ou seja digitados 9 caracteres
+    // Faz o laÃ§o enquanto alguem venÃ§a ou seja digitados 9 caracteres
     while((cont == 0)&& (nenhum<9)){
         system("cls");
         cout << endl << joga1 << " =  X";
         cout << endl << joga2 << " =  0";
         mapa(teste);
         mapa_mod(velha);
-        // Lê a posição do jogador 1
+        // LÃª a posiÃ§Ã£o do jogador 1
         cout << joga1 << " digite a posicao que deseja jogar: ";
         cin >> posicao;
-        // Verifica se a posição digitada ja existe
+        // Verifica se a posiÃ§Ã£o digitada ja existe
         temp = (verifica_posicao(velha,posicao));
         while(temp == 1){
             cout << endl << "Vc digitou uma posicao ocupada!";
@@ -87,7 +87,7 @@ int main(){
             cin >> posicao;
             temp = verifica_posicao(velha,posicao);
         }
-        // Insere a posição na tabela
+        // Insere a posiÃ§Ã£o na tabela
         if(posicao == '1') velha[2][0] = 'x';
         if(posicao == '2') velha[2][1] = 'x';
         if(posicao == '3') velha[2][2] = 'x';
@@ -130,9 +130,9 @@ int main(){
             vencedor = 1,cont = 1,termina = 1;
         if(velha[0][0]=='x' && velha[1][1]=='x' && velha[2][2]=='x' )
             vencedor = 1,cont = 1,termina = 1;
-        // Caso o jogador 1 ganhar, finaliza o laço.
+        // Caso o jogador 1 ganhar, finaliza o laÃ§o.
         if(termina==1) break;
-        // Soma 1  a nenhum... (caso não haja vencedor)
+        // Soma 1  a nenhum... (caso nÃ£o haja vencedor)
         nenhum++;
         // Verifica se ja aconteceram 9 rodadas
         if (nenhum==9) break;
@@ -143,7 +143,7 @@ int main(){
         mapa_mod(velha);
         cout << joga2 << " digite a posicao que deseja jogar: ";
         cin >> posicao;
-        // Verifica se a posição digitada ja foi usada
+        // Verifica se a posiÃ§Ã£o digitada ja foi usada
         temp = verifica_posicao(velha,posicao);
         while(temp == 1){
             cout << endl << "Vc digitou uma posicao ocupada!";
@@ -151,7 +151,7 @@ int main(){
             cin >> posicao;
             temp = verifica_posicao(velha,posicao);
         }
-        // Insere posição na tabela
+        // Insere posiÃ§Ã£o na tabela
         if(posicao == '1') velha[2][0] = '0';
         if(posicao == '2') velha[2][1] = '0';
         if(posicao == '3') velha[2][2] = '0';
@@ -202,7 +202,7 @@ int main(){
         nenhum++;
         if (nenhum==9) break;
     }
-    // Apos o laço mostra o resultado correspondente
+    // Apos o laÃ§o mostra o resultado correspondente
     if(vencedor == 1){
         system("cls");
         mapa_mod(velha);
